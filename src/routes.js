da25c11ea729420d53f9fc7bdb64f3c80d7d1323
fediaz3 <fediaz3@uc.cl>
2021-04-30 18:06:24 -0400

    routerArtistsStep1

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,16 @@ const KoaRouter = require('koa-router');
 const hello = require('./routes/hello');
 const index = require('./routes/index');
 
+// new models:
+const artists = require('./routes/artists.js');
+// end new-models
+
 const router = new KoaRouter();
 
 router.use('/', index.routes());
 router.use('/hello', hello.routes());
+// new models:
+router.use('/artists', artists.routes());
+// end new-models
 
 module.exports = router;
---
+++ src/routes/artists.js
@@ -0,0 +1,22 @@
+const KoaRouter = require('koa-router'); // para pedir la libreria koa-router
+const router = new KoaRouter();          // creo un router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;                 // exporto el router (AL FINAL DEL ARCHIVO SIEMPRE)
